42953ca514be8ba5d4c6d8cec468d606c729fc07
Trung Pham Duc <trung.phamduc@asnet.com.vn>
2015-07-22 09:28:14 +0700

    Added calendar settings struct

---
+++ Calendar/CalendarKit/Settings.swift
@@ -0,0 +1,42 @@
+//
+//  Settings.swift
+//  Calendar
+//
+//  Created by Trung Pham on 7/21/15.
+//  Copyright (c) 2015 Lancy. All rights reserved.
+//
+
+import UIKit
+
+struct ASSettings {
+  var monthFormat: String
+  var monthRange: Int
+  var dateSelectedImg: UIImage?
+  
+  var weekBgColor: String
+  var weekFont: String
+  var weekFontSize: CGFloat
+  var weekTxtColor: String
+  
+  var dateTxtColor: String
+  var dateFont: String
+  var dateFontSize: CGFloat
+  var dateAlpha: CGFloat
+  
+  
+}
+
+// default calendar settings
+var calendarSettings = ASSettings(
+  monthFormat: "LLLL yyyy",
+  monthRange: 24,
+  dateSelectedImg: nil,
+  weekBgColor: "#fff",
+  weekFont: "Helvetica Neue",
+  weekFontSize: 11,
+  weekTxtColor: "#000",
+  dateTxtColor: "#000",
+  dateFont: "Helvetica Neue",
+  dateFontSize: 15,
+  dateAlpha: 0.4
+)
